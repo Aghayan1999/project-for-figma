2eec94842fc6c7941b659a17480ae7a1e512bce8
Aghayan1999 <levon.agayan@inbox.ru>
2022-05-24 22:28:39 +0400

    add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ const watch=require('gulp-watch');
 const gulpSass=require('gulp-sass');
 const dartSass=require('sass');
 const sass=gulpSass(dartSass);
-
+const autoprefixer=require('gulp-autoprefixer');
+const sourcemaps=require('gulp-sourcemaps')
 gulp.task('watch',function(){
     watch(['./build/*.html','./build/css/**/*.css'],gulp.parallel(browsersync.reload))
 })
@@ -20,9 +21,14 @@ gulp.task('server',function(){
 
 gulp.task('scss',function(callback){
     return gulp.src('./sass/style.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(autoprefixer({
+        overrideBrowserslid:['last 3 versions']
+    }))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./build/css'))
      callback()
 })
 
-gulp.task('default',gulp.parallel('server','watch'))
+gulp.task('default',gulp.parallel('server','watch','scss'))
