19e2cdf66cd9bc7f580455d9b73c1e443b1ed00e
Aghayan1999 <levon.agayan@inbox.ru>
2022-05-24 21:59:11 +0400

    add watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,12 @@
 const gulp=require('gulp');
+const browsersync=require('browser-sync').create();
+
+
+gulp.task('server',function(){
+    browsersync.init({
+        server:{
+            baseDir:'./build/'
+        }
+    })
+})
 
