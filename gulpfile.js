2c895923a52ebe427d8eaf2471a58e0482c84f0c
Aghayan1999 <levon.agayan@inbox.ru>
2022-05-24 22:21:33 +0400

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp=require('gulp');
 const browsersync=require('browser-sync').create();
 const watch=require('gulp-watch');
+const gulpSass=require('gulp-sass');
+const dartSass=require('sass');
+const sass=gulpSass(dartSass);
 
 gulp.task('watch',function(){
     watch(['./build/*.html','./build/css/**/*.css'],gulp.parallel(browsersync.reload))
@@ -12,6 +15,14 @@ gulp.task('server',function(){
             baseDir:'./build/'
         }
     })
+})
+
+
+gulp.task('scss',function(callback){
+    return gulp.src('./sass/style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./build/css'))
+     callback()
 })
 
 gulp.task('default',gulp.parallel('server','watch'))
